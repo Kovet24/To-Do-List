b1ecd8c509e590fa1052388f67edbd1c54b1392f
Konstantin <the.kovet24@gmail.com>
2025-03-16 15:01:38 +0300

    Add description of Priority

--- src/model/Priority.java
+++ src/model/Priority.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.NoSuchElementException;
+
 public enum Priority {
-    LOW, MEDIUM, HIGH
+    LOW("Low"), MEDIUM("Medium"), HIGH("High");
+
+    private final String description;
+
+    Priority(String description) {
+        this.description = description;
+    }
+
+    public static Priority getPriority(String description) {
+        return switch (description.toLowerCase()) {
+            case "low" -> Priority.LOW;
+            case "medium" -> Priority.MEDIUM;
+            case "high" -> Priority.HIGH;
+            default -> throw new NoSuchElementException("Not such priority");
+        };
+    }
 }
--- src/model/Record.java
+++ src/model/Record.java
@@ -4,15 +4,27 @@ import java.time.LocalDate;
 
 public class Record {
     private String name;
+    private Priority priority;
     private Status status;
     private String description;
     private final LocalDate dateOfCreate;
     private LocalDate dateOfLastChanges;
 
-    public Record(String name, String description) {
+    public Record(String name, Priority priority, String description) {
         this.name = name;
+        this.priority = priority;
         status = Status.NOT_STARTED;
         dateOfCreate = LocalDate.now();
         dateOfLastChanges = LocalDate.now();
     }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
