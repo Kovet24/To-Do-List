b28cd2138b0b9dfc8ec46c64cf0075ca5c66af6c
Konstantin <the.kovet24@gmail.com>
2025-03-25 15:03:50 +0300

    Add new methods at UserService to set Record values

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -27,75 +27,32 @@ public class UserService {
 
             switch (choice) {
                 case "1":
-                    String newName = scanner.nextLine();
-                    toDoListService.setNameOfRecord(name, newName);
-                    System.out.println("Название изменено.\n");
+                    name = setName(name, scanner);
                     printMenu();
-
                     break;
 
                 case "2":
-
-                    System.out.println("Выберите приоритет:");
-                    System.out.println("1. High;\n2. Medium;\n3. Low;");
-
-                    String newPriority = scanner.nextLine();
-
-                    switch (newPriority.toLowerCase()) {
-                        case "high":
-                            toDoListService.setPriorityOfRecord(name, Priority.HIGH);
-                            break;
-                        case "medium":
-                            toDoListService.setPriorityOfRecord(name, Priority.MEDIUM);
-                            break;
-                        case "low":
-                            toDoListService.setPriorityOfRecord(name, Priority.LOW);
-                            break;
-                    }
-                    System.out.println("Приоритет изменен.\n");
+                    setPriority(name, scanner);
                     printMenu();
-
                     break;
 
                 case "3":
-                    System.out.println("Выберете статус:");
-                    System.out.println("1. Done;\n2. In process;\n3. Not started;");
-
-                    String newStatus = scanner.nextLine();
-
-                    switch (newStatus.toLowerCase()) {
-                        case "done":
-                            toDoListService.setStatusOfRecord(name, Status.DONE);
-                            break;
-                        case "in process":
-                            toDoListService.setStatusOfRecord(name, Status.IN_PROCESS);
-                            break;
-                        case "not started":
-                            toDoListService.setStatusOfRecord(name, Status.NOT_STARTED);
-                            break;
-                    }
-                    System.out.println("Статус изменен.\n");
+                    setStatus(name, scanner);
                     printMenu();
-
                     break;
 
                 case "4":
-                    System.out.println("Введите новое описание:");
-
-                    String newDescription = scanner.nextLine();
-                    toDoListService.setDescriptionOfRecord(name, newDescription);
-                    System.out.println("Описание изменено.\n");
+                    setDescription(name, scanner);
                     printMenu();
-
                     break;
 
                 case "5":
                     isStop = true;
                     break;
+
                 default:
                     System.out.println("Ошибка!\n");
                     printMenu();
-
                     break;
             }
         }
@@ -105,6 +62,63 @@ public class UserService {
 
     private void printMenu() {
         System.out.println("Выберете изменение:");
-        System.out.println("1. Название;\n2. Приоритет;\n3. Статус;\n4. Описание;\n5. Закончить изменения.");
+        System.out.println("1. Название;\n2. Приоритет;\n3. Статус;\n4. Описание;\n5. Закончить изменения.\n");
+    }
+
+    private String setName(String name, Scanner scanner) {
+        System.out.println("Введиете новое название:");
+        String newName = scanner.nextLine();
+        toDoListService.setNameOfRecord(name, newName);
+        System.out.println("Название изменено.\n");
+
+        return newName;
+    }
+
+    private void setPriority(String name, Scanner scanner) {
+        System.out.println("Выберите приоритет:");
+        System.out.println("1. High;\n2. Medium;\n3. Low;");
+
+        String newPriority = scanner.nextLine();
+
+        switch (newPriority.toLowerCase()) {
+            case "1":
+                toDoListService.setPriorityOfRecord(name, Priority.HIGH);
+                break;
+            case "2":
+                toDoListService.setPriorityOfRecord(name, Priority.MEDIUM);
+                break;
+            case "3":
+                toDoListService.setPriorityOfRecord(name, Priority.LOW);
+                break;
+        }
+        System.out.println("Приоритет изменен.\n");
+    }
+
+    private void setStatus(String name, Scanner scanner) {
+        System.out.println("Выберете статус:");
+        System.out.println("1. Done;\n2. In process;\n3. Not started;");
+
+        String newStatus = scanner.nextLine();
+
+        switch (newStatus.toLowerCase()) {
+            case "1":
+                toDoListService.setStatusOfRecord(name, Status.DONE);
+                break;
+            case "2":
+                toDoListService.setStatusOfRecord(name, Status.IN_PROCESS);
+                break;
+            case "3":
+                toDoListService.setStatusOfRecord(name, Status.NOT_STARTED);
+                break;
+        }
+        System.out.println("Статус изменен.\n");
+    }
+
+    private void setDescription(String name, Scanner scanner) {
+        System.out.println("Введите новое описание:");
+
+        String newDescription = scanner.nextLine();
+        toDoListService.setDescriptionOfRecord(name, newDescription);
+        System.out.println("Описание изменено.\n");
     }
 }
